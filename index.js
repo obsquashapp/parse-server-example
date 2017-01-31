81a7e23c51a1eb390e09af74db3eaf604e866e84
obsquashapp <obsquashapp@gmail.com>
2017-01-31 11:05:00 +0100

    Update index.js
    added postmark mail adapter settings

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+verifyUserEmails: true, // Enable email verification
+  emailAdapter: PostmarkAdapter({
+    apiKey: 'c1fc1e63-7957-4498-8f1d-aeee37193d3b',
+    fromAddress: 'obsquash@obsquashapp.com',
+  }),
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
