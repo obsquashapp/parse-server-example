0baf211967c7b23179f99dc0f32ed88280253c46
obsquashapp <obsquashapp@gmail.com>
2017-01-31 12:09:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,17 +23,17 @@ verifyUserEmails: true, // Enable email verification
   emailAdapter: {
         module: "simple-parse-smtp-adapter",
         options: {
-            fromAddress: 'your@sender.address',
-            user: 'email@email.com',
-            password: 'AwesomePassword',
-            host: 'your.smtp.host',
+            fromAddress: 'obsquashapp@gmail.com',
+            user: 'obsquashapp@gmail.com',
+            password: '8terlich4us',
+            host: 'smtp.gmail.com',
             isSSL: true, //True or false if you are using ssl 
-            port: 465, //SSL port or another port 
-            name: 'your domain name', //  optional, used for identifying to the server  
+            port: 587, //SSL port or another port 
+            name: 'domain name', //  optional, used for identifying to the server  
             //Somtimes the user email is not in the 'email' field, the email is search first in 
             //email field, then in username field, if you have the user email in another field 
             //You can specify here 
-            emailField: 'username', 
+            emailField: 'obsquashapp@gmail.com', 
             templates: {
                 //This template is used only for reset password email 
                 resetPassword: {
